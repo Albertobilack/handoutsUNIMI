ed9ba514cdb2656aa5044e1f2a5c26cf99601aac
Albertobilack <binoalberto02@gmail.com>
2025-04-14 23:16:03 +0200

    sync

--- src/main/java/it/unimi/di/prog2/e06/Combine.java
+++ src/main/java/it/unimi/di/prog2/e06/Combine.java
@@ -1,36 +1,92 @@
 package it.unimi.di.prog2.e06;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * test
+ * utility class for CombineClient
  */
 public class Combine {
 
-    /** PLACEHOLDER */
-    private Combine () {}
+    /**PLACEHOLDER */
+    private Combine() {}
 
     /**
-     * Modify {@code a} by multipling each of his elements by the sum of
-     * elements in {@code b}
-     *
-     * @param a PLACEHOLDER
-     * @param b PLACEHOLDER
-     * @throws NullPointerException if a == {@code null} or b == {@code null}
+     * return the sum of the given list
+     * 
+     * @param container contains the numbers whom sum must be computed
+     * @throws IllegalArgumentException if {@code container} is empty
      */
-    public static void combineValues(int[] a, int[] b) throws NullPointerException {
+    public static int sumList(List<Integer> container) throws IllegalArgumentException {
 
-        if (a == null || b == null) {
-            throw new NullPointerException();
+        if (container.size() == 0) {throw new IllegalArgumentException();}
+
+        int sum = 0;
+        for(int i : container) {
+            sum+= i;
         }
 
-        if (a.length != 0 && b.length != 0) {
-            int sumB = 0;
-            for (int value : b) {
-                sumB += value;
-            }
-            for (int i = 0; i < a.length; i++) {
-                a[i] = a[i] * sumB;
-            }
+        return sum;
+    }
+
+
+    //dobbiamo decidere se modificare container o ritornarne uno nuovo
+    //preferisco ritornarne uno nuovo
+    //e se container è vuoto? ritorniamo una lista vuota
+    /**
+     * return a new List containing each element of {@code container} multiplied by {@code multip}
+     * 
+     * <p> the returned list will have the same order as {@code container}.
+     * returns an empty list if {@code container} is empty
+     * 
+     * @param containers contains elements that must be multiplied with {@code multip}    
+     * @param multip the multiplier
+     * 
+     */
+    public static List<Integer> multiplyList(List<Integer> container, int multip) {
+        
+        List<Integer> multipList = new ArrayList<>();
+        
+        for(int i : container) {
+            multipList.add(i*multip);
         }
+        
+        return multipList;
+
     }
 
 }
+
+
+//
+//public class Combine {
+//
+//    /** PLACEHOLDER */
+//    private Combine () {}
+//
+//    /**
+//     * Modify {@code a} by multipling each of his elements by the sum of
+//     * elements in {@code b}
+//     *
+//     * @param a PLACEHOLDER
+//     * @param b PLACEHOLDER
+//     * @throws NullPointerException if a == {@code null} or b == {@code null}
+//     */
+//    public static void combineValues(int[] a, int[] b) throws NullPointerException {
+//
+//        if (a == null || b == null) {
+//            throw new NullPointerException();
+//        }
+//
+//        if (a.length != 0 && b.length != 0) {
+//            int sumB = 0;
+//            for (int value : b) {
+//                sumB += value;
+//            }
+//            for (int i = 0; i < a.length; i++) {
+//                a[i] = a[i] * sumB;
+//            }
+//        }
+//    }
+//
+//}
+//
--- src/main/java/it/unimi/di/prog2/e06/CombineClient.java
+++ src/main/java/it/unimi/di/prog2/e06/CombineClient.java
@@ -3,6 +3,7 @@ package it.unimi.di.prog2.e06;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * Esercizio 4.4 di PDJ.
@@ -11,80 +12,163 @@ import java.util.Scanner;
  * a=[9, 18, 27]. What should this procedure do if a or b is null or empty? Give
  * a specification for combine that answer these questions and explain why your
  * specification is a good one.
+ * 
+ * RAGIONAMENTO: 
+ * 
+ * 
  */
 
 public class CombineClient {
 
-    /**
-     * .
-     */
-    private CombineClient() {
-    }
+    /** PLACEHOLDER */
+    private CombineClient() {}
 
     /**
-     * Decodifica una stringa contenente interi separati da spazi.
-     *
-     * @param string la stringa in ingresso, non può essere {@code null} e deve
-     * contenere interi separati da spazi
-     *
-     * @return gli interi contenuti nella stringa.
-     *
-     */
-    private static int[] parseInts(String string) {
-        List<Integer> list = new ArrayList<>();
-        try (Scanner sl = new Scanner(string)) {
-            while (sl.hasNextInt()) {
-                list.add(sl.nextInt());
-            }
-        }
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
-        return result;
-    }
-
-    /**
-     * Used to test combineValues method from Combine class.
-     * Read from {@code system.in} two lines containing integers separated by spaces,
-     * and modify the values of each element of the first line {@code a} by
-     * multipling them by the sum of the elements of the second line {@code B}
-     * If {@code A} or {@code B} is null, an exception is raised, if system.in
-     * contains non-integer input, an exception is raised
-     *
-     * (se sono vuoti è accettabile)
-     * atparametro system.in used to read {@code A} and {@code B}
-     *
+     * given a sequence of integer a and b from input stream, print
+     * on console the the result of the multiplication of each element of a
+     * for the sum of all elements of b
+     * 
+     * <p> at least one integer must be provided for b. 
+     * read two rows, row 1 must contains elements of a, row 2 must contains elements of b
+     * 
+     * 
      * @param args not used
-     * @throws IllegalArgumentException if input is incorrect or scanner fails 
+     * throws IllegalArgumentException if {@code system.in} contains input != integer
+     * throws IllegalArgumentException if no arguments are given for b
+     * throws 
      */
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) throws IllegalArgumentException{
 
-        int[] a;
-        int[] b;
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
 
-        try (Scanner s = new Scanner(System.in)) {
-            a = parseInts(s.nextLine());
-            b = parseInts(s.nextLine());
-        } catch (RuntimeException e) { //catch illegalstate and nosuchelement
-            throw new IllegalArgumentException("exception reading input lines on main method class CombineClient");
+        String containerA = "";
+        String containerB = "";
+        try(Scanner s = new Scanner(System.in)) {
+            containerA = s.nextLine();
+            containerB= s.nextLine();
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(); //da cambiare non ricordo exception che lancia lo scanner
         }
 
         try {
-            Combine.combineValues(a, b);
-        } catch (NullPointerException e) { //should not happen so maybe I can avoid it
-            throw new NullPointerException("exception calling combineValues on main method class CombineClient");
+            //implementazione sbagliata
+            //a = (Arrays.asList(Integer.getInteger(containerA.split(" "))));
+
+            String[] temp = containerA.split(" ");
+            for (String i : temp) {
+                a.add(Integer.parseInt(i));
+            }
+
+            temp = containerB.split(" ");
+            for (String i : temp) {
+                b.add(Integer.parseInt(i));
+            }
+
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("CombineClient.main");
         }
 
-        for (int value : a) {
-            System.out.println(value);
+
+        int multip = 0;
+        try {
+            multip = Combine.sumList(b);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("CombineClient.main");
+        }
+
+        List<Integer> result = Combine.multiplyList(a, multip);
+
+        for (int i : result) {
+            System.out.println(i);
         }
 
     }
 
-    // Il main di questa classe legge due righe dal flusso di ingresso ciascuna
-    // delle quali contiene gli interi (separati da spazi) di uno dei due array da
-    // combinare e ne emette il risultato della combinazione (separando gli interi
-    // uno per linea). Può avvalersi della funzione precedente per decodificare
-    // ciascuna delle due linee in ingresso.
+
 }
+
+
+
+
+
+
+
+// RIFATTO
+
+//public class CombineClient {
+//
+//    /**
+//     * .
+//     */
+//    private CombineClient() {
+//    }
+//
+//    /**
+//     * Decodifica una stringa contenente interi separati da spazi.
+//     *
+//     * @param string la stringa in ingresso, non può essere {@code null} e deve
+//     * contenere interi separati da spazi
+//     *
+//     * @return gli interi contenuti nella stringa.
+//     *
+//     */
+//    private static int[] parseInts(String string) {
+//        List<Integer> list = new ArrayList<>();
+//        try (Scanner sl = new Scanner(string)) {
+//            while (sl.hasNextInt()) {
+//                list.add(sl.nextInt());
+//            }
+//        }
+//        int[] result = new int[list.size()];
+//        for (int i = 0; i < list.size(); i++) {
+//            result[i] = list.get(i);
+//        }
+//        return result;
+//    }
+//
+//    /**
+//     * Used to test combineValues method from Combine class.
+//     * Read from {@code system.in} two lines containing integers separated by spaces,
+//     * and modify the values of each element of the first line {@code a} by
+//     * multipling them by the sum of the elements of the second line {@code B}
+//     * If {@code A} or {@code B} is null, an exception is raised, if system.in
+//     * contains non-integer input, an exception is raised
+//     *
+//     * (se sono vuoti è accettabile)
+//     * atparametro system.in used to read {@code A} and {@code B}
+//     *
+//     * @param args not used
+//     * @throws IllegalArgumentException if input is incorrect or scanner fails 
+//     */
+//    public static void main(String[] args) throws IllegalArgumentException {
+//
+//        int[] a;
+//        int[] b;
+//
+//        try (Scanner s = new Scanner(System.in)) {
+//            a = parseInts(s.nextLine());
+//            b = parseInts(s.nextLine());
+//        } catch (RuntimeException e) { //catch illegalstate and nosuchelement
+//            throw new IllegalArgumentException("exception reading input lines on main method class CombineClient");
+//        }
+//
+//        try {
+//            Combine.combineValues(a, b);
+//        } catch (NullPointerException e) { //should not happen so maybe I can avoid it
+//            throw new NullPointerException("exception calling combineValues on main method class CombineClient");
+//        }
+//
+//        for (int value : a) {
+//            System.out.println(value);
+//        }
+//
+//    }
+//
+//    // Il main di questa classe legge due righe dal flusso di ingresso ciascuna
+//    // delle quali contiene gli interi (separati da spazi) di uno dei due array da
+//    // combinare e ne emette il risultato della combinazione (separando gli interi
+//    // uno per linea). Può avvalersi della funzione precedente per decodificare
+//    // ciascuna delle due linee in ingresso.
+//}
+//
